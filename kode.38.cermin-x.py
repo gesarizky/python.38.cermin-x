1a8639be63a27d35fcc9605464010f47f22d0851
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-19 13:11:55 +0700

    Add files via upload

---
+++ kode.38.cermin-x.py
@@ -0,0 +1,17 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+x = float(input("Tuliskan Nilai X : "))
+y = float(input("Tuliskan Nilai Y: "))
+
+# Mengkonversi
+x1 = y
+x2 = x 
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka X* = %0.2f'  %(x1))
+print('Maka Y* = %0.2f'  %(x2))
+print('==========================================================')
